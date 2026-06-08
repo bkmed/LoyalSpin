01e08266170a0ce14c0db74d487905551382e94f
mohamed ben khedher <mohamed.benkhedher@mobile-spot.com>
2026-06-08 13:53:22 +0100

    u assets

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './build'),
     filename: 'index_bundle.js',
-    publicPath: process.env.NODE_ENV === 'production' ? '/Plombier/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/LoyalSpin/' : '/',
     clean: true,
   },
   devServer: {
